e4c91aaa20c4a873160cb7e7fb791a4520389674
Robert Rothenberg <rrwo@cpan.org>
2026-02-13 23:36:24 +0000

    Add stub POD

--- lib/Apache/Session/Generate/Random.pm
+++ lib/Apache/Session/Generate/Random.pm
@@ -9,6 +9,29 @@ use Crypt::SysRandom ();
 
 our $VERSION = '0.001000';
 
+# ABSTRACT: use system randomness for generating session ids
+
+=head1 SYNOPSIS
+
+    use Apache::Session::Flex;
+
+    tie %sessions, 'Apache::Session::Flex', $id, {
+        Store     => 'Postgres',
+        Lock      => 'Null',
+        Generate  => 'Random',
+        Serialize => 'Base64',
+    };
+
+=head1 DESCRIPTION
+
+This module extends L<Apache::Session> to create secure random session ids using the system's source of randomness.
+
+=for Pod::Coverage generate
+
+=for Pod::Coverage validate
+
+=cut
+
 sub generate {
     my ($session) = @_;
     return $session->{'data'}->{'_session_id'} = unpack( 'H*', Crypt::SysRandom::random_bytes(20) );
